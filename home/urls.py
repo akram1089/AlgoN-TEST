26a2557b0106b14b3261f1df339f8797d4c9577d
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2023-05-22 12:39:31 +0530

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('delete_user/<str:id>',views.delete_user,name='delete_user'),
     path('delete_feedback/<str:id>',views.delete_feedback,name='delete_feedback'),
 ]
- urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
 from django.conf import settings
 from django.conf.urls.static import static
 
