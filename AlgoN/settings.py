1a37e5130cc545f8238ac4311aa6b222f6215e82
Tufail Akram <72789060+akram1089@users.noreply.github.com>
2023-05-22 15:40:12 +0530

    debug=true

--- AlgoN/settings.py
+++ AlgoN/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_aei3ye21%7gpl=n-c175nj4+^d7cs)59@l^sudk(7$mr3)8s^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
